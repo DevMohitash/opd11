42ee38cc3a2b1e0d3e100eaa3765ee8a1eef0800
Mohitash Kumar <devmohitashxda@gmail.com>
2022-04-27 15:51:12 +0530

    Add files via upload

---
+++ filses/mkadpanim1000.sh
@@ -0,0 +1,10 @@
+ #!/system/bin/sh
+#=======================================#
+#VARIABLES===============================#
+BB=/data/adb/magisk/busybox
+CLOG=/data/media/0/Android/.libbin/.mkadpshshsh/YourBLchanges.log
+#=======================================#
+#=======================================#
+settings put global window_animation_scale 10
+settings put global transition_animation_scale 10
+settings put global animator_duration_scale 10
