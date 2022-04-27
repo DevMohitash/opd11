de393beb51bca0ec61d592edfc4a0c6c8d19b7a7
Mohitash Kumar <devmohitashxda@gmail.com>
2022-04-27 16:02:17 +0530

    Add files via upload

---
+++ filses/mkadprcvry.sh
@@ -0,0 +1,8 @@
+ #!/system/bin/sh
+#=======================================#
+#VARIABLES===============================#
+BB=/data/adb/magisk/busybox
+CLOG=/data/media/0/Android/.libbin/.mkadpshshsh/YourBLchanges.log
+#=======================================#
+#=======================================#
+svc power reboot recovery
